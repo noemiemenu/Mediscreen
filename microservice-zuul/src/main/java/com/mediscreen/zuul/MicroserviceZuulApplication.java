fc0e1679e86695d278503e1c3e05ee8b0010e343
Noemie Menu <menunoemie.dev@outlook.com>
2021-09-26 20:45:33 +0200

    add zuul microservice

---
+++ microservice-zuul/src/main/java/com/mediscreen/zuul/MicroserviceZuulApplication.java
@@ -0,0 +1,15 @@
+package com.mediscreen.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class MicroserviceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceZuulApplication.class, args);
+    }
+
+}
